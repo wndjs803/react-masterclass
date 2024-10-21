fbcae51da942a70d7f25fdb6c1cfe24e1706e049
김주언 <wndjs803@naver.com>
2024-10-21 17:12:40 +0900

    styled components theme

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  from {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  to {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 100px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😇</Emoji>
-        <span style={{ color: "white" }}>sdfsdfdsfsd</span>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+
+const dartThem = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightThem = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={dartThem}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
