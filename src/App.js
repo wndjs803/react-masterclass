b263e132de32c85a260833c2a57fd5d7b4c94ae0
김주언 <wndjs803@naver.com>
2024-10-21 14:05:16 +0900

    styled-components as, attrs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Circle bgColor="teal" />
-      <Box bgColor="tomato" />
+    <Father as="header">
+      <Input></Input>
     </Father>
   );
 }
