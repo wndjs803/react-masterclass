1568247dbe433bd6e22278192c65df0cc1748ab3
김주언 <wndjs803@naver.com>
2024-10-22 10:56:19 +0900

    change version to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,9 @@ const lightThem = {
   backgroundColor: "whitesmoke",
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={dartThem}>
