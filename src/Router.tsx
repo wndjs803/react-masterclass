78515e1cd7bd4be741d4642cb910f35681501fe0
김주언 <wndjs803@naver.com>
2024-10-24 01:35:52 +0900

    errorElement

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import About from "./screen/About";
 import Home from "./screen/Home";
+import NotFound from "./screen/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
 const router = createBrowserRouter([
   {
@@ -12,12 +14,14 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+        errorElement: <ErrorComponent />,
       },
       {
         path: "about",
         element: <About />,
       },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 
--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,5 +1,6 @@
 function Home() {
-  return <h1>Home</h1>;
+  const users: any = [];
+  return <h1>{users[0].name}</h1>;
 }
-
+// users에는 name이라는 property가 없기 때문에 에러가 발생함
 export default Home;
