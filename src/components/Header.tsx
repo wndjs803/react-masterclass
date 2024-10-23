affcbb2cae96a101782ce212888926d1445fe9bb
김주언 <wndjs803@naver.com>
2024-10-24 01:41:17 +0900

    useNavigate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about");
+  };
   return (
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to="/about">About</Link>
+        <button onClick={onAboutClick}>About</button>
       </li>
     </ul>
   );
