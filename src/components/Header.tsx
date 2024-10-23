ff4a138103d5b18ce1c9bd6d9964e48f5989b9dd
김주언 <wndjs803@naver.com>
2024-10-24 01:14:40 +0900

    react-dom 6.4 BrowserRouter

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Header;
