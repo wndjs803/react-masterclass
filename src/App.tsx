732a4506c923a558695e2d63ce4bf9626a272cd3
김주언 <wndjs803@naver.com>
2024-11-02 00:27:03 +0900

    motionValues part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
 import { click } from "@testing-library/user-event/dist/click";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -30,33 +30,16 @@ const BiggerBox = styled.div`
   overflow: hidden; // 해당 컴포넌트를 넘어가면 안보이게
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: {
-    backgroundColor: "rgb(46, 204, 113)", // 숫자값으로 색 지정하면 천천히 색 바뀜
-    transition: {
-      duration: 10,
-    },
-  },
-};
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0); // x값 추적, state가 아님, 리렌더링 되지 않음
+  // console.log(x); // 그래서 한 번만 출력
+  useEffect(() => {
+    x.onChange(() => console.log(x.get()));
+  }, [x]);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin // 가운데로 옮기는 힘
-          dragElastic // 기본값 0.5 1로 갈수록 마우스 포인터대로 움직임
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-          whileDrag="drag"
-        />
-      </BiggerBox>
+      <button onClick={() => x.set(200)}>Click Me</button>
+      <Box style={{ x }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
