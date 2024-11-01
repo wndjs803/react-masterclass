1d47c5154da380334ac0d356e18d5d0e4ac2edfc
김주언 <wndjs803@naver.com>
2024-11-01 22:36:48 +0900

    variants, delayChildren, staggerChildren

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,61 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVariants = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  background-color: white;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 1,
+      bounce: 0.5,
+      delayChildren: 0.5, // 자식 애니메이션을 0.5초만큼 지연시킨다.
+      staggerChildren: 0.2, // 자식 애니메이션을 0.2가 지날때마다 하나씩 진행시킨다.
+    },
+  },
 };
 
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+// start, end를 일치시켜야 자동으로 인식해서 적용한다.
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVariants} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        {/* 자동으로 자식까지 애니메이션이 전파된다. */}
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
